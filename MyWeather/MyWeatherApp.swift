5cbadf9f4aad61f8ace6bbf315c7c996a78785f8
Prim <prima.putra02@gmail.com>
2024-09-24 10:04:18 +0700

    Initial Commit

---
+++ MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by ichiro on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
